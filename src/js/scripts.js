f62a80bd1ecc0eb8081c9d21743540ebf7c51686
Kristin Fast <kmfast@gmail.com>
2020-09-21 11:51:02 -0600

    Kristin's code from time w Casey

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,23 @@
   'use strict';
 
   $(function () {
-
+    $('input[name=payment]').on('change', function() {
+      $('input[name=payment]').removeAttr('checked', 'checked');
+      $(this).attr('checked', 'checked');
+      console.log($(this).val());
+      console.log(this);
+      if($(this).val() === 'monthly') {
+        console.log('monthly');
+        $('#monthly-pricing').show();
+        $('#yearly-pricing').hide();
+        $('input[name="monthly"]').prop("checked", true);
+      } else {
+        console.log('yearly');
+        $('#monthly-pricing').hide();
+        $('#yearly-pricing').show();
+        $('input[name="yearly"]').prop("checked", true);
+      }
+    });
   });
 
 })(jQuery, window, document);
