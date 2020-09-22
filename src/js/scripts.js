b67d788a0f9890b98c35f57ea0ed8527bfe1631f
Lia Duguran <liaduguran@gmail.com>
2020-09-22 14:51:17 -0700

    replicate price table
    :WP zz

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,24 @@
 
   $(function () {
 
+
+
+    $( ".choose-price" ).click(function() {
+        var val = $(this).val();
+        if(val === 'yearly'){
+          $(".pricing-table-yearly").addClass("open");
+          $( ".pricing-table" ).removeClass( "open" )
+        }else if(val === 'monthly'){
+          $(".pricing-table").addClass("open");
+          $( ".pricing-table-yearly" ).removeClass( "open" )
+          console.log('monthly');
+        }
+    });
+    
+    // let addText = document.createTextNode('Free Trial'),
+    // trialTxt = document.getElementById("trial");
+    // trialTxt.appendChild(addText);
+
   });
 
 })(jQuery, window, document);
