73d84a56cf45be3e0044ce552d53fcd828a297aa
FastKristin <kmfast@gmail.com>
2020-09-21 21:14:46 -0600

    Generating mobile view; JS for interaction (iteration 1, anyway); some styling; some layout cleanup. Time: ~1 hour (1h3m)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,31 @@
 (function ($, window, document, undefined) {
 
   'use strict';
-  
+
 
   $(function () {
     $('input[name=payment]').on('change', function() {
       $('input[name=payment]').removeAttr('checked', 'checked');
       $(this).attr('checked', 'checked');
-      console.log($(this).val());
-      console.log(this);
       if($(this).val() === 'monthly') {
-        console.log('monthly');
         $('#monthly-pricing').show();
         $('#yearly-pricing').hide();
         $('input[name="monthly"]').prop("checked", true);
       } else {
-        console.log('yearly');
         $('#monthly-pricing').hide();
         $('#yearly-pricing').show();
         $('input[name="yearly"]').prop("checked", true);
+      }
+    });
+
+    $('.accordion-top').on('click', function() {
+      if($(this).siblings('.accordion-body').hasClass('open')) {
+        $(this).siblings('.accordion-body').removeClass('open');
+        $('.accordion-top').removeClass('rotate');
+      } else {
+        $('.accordion-body').removeClass('open');
+        $(this).siblings('.accordion-body').addClass('open');
+        $(this).addClass('rotate');
       }
     });
   });
