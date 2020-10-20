946fa9df27edbb6c6e0f52f528dd940c3a7ff85f
RedLucas <lucasr@adcisorwebsites.com>
2020-10-20 14:59:52 -0700

    Create a pricing section and add a radio toggle.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,24 @@
 (function ($, window, document, undefined) {
 
   'use strict';
-
   $(function () {
-
+    const $radioToggle = $('[data-radio-toggle]');
+    const selector = $radioToggle.data('radioToggle');
+    const $radioToggled = $(selector);
+    if ($radioToggled.length !== 0) {
+      const $radio = $radioToggle.find('[data-toggle]');
+      $radio.on('checked', function() {
+        const radioSelection = $(this).data('toggle');
+        const $section = $radioToggled.find(radioSelection);
+        $section.removeClass('d-none');
+        $radio.forEach(element => {
+          if (element.prop('checked') !== TRUE) {
+            const $section = $radioToggled.find(element.data('toggle'));
+            $section.addClass('d-none');
+          }
+        });
+      });
+    }
   });
 
 })(jQuery, window, document);
